6fd68abb2b36d4a473ee0c4536957c34d14f901e
luch91 <luchijudith@gmail.com>
2026-05-29 09:26:38 +0100

    fix: remove non-existent crawlQueue export, exclude worker.ts from crawler build

--- packages/crawler/src/index.ts
+++ packages/crawler/src/index.ts
@@ -1,5 +1,5 @@
 export { DomainCrawler, crawlDomain, type CrawlOptions, type CrawlEvents } from './crawler';
-export { crawlQueue, enqueueCrawlJob, redisConnection, type CrawlJobData } from './queue';
+export { enqueueCrawlJob, redisConnection, type CrawlJobData } from './queue';
 export { RobotsParser, fetchRobotsTxt, type RobotsRules } from './robots';
 export { fetchSitemapUrls } from './sitemap';
 export { extractChunks, type PageChunk } from './extractor';
